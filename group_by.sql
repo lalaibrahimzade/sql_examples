69c943a1749449701069a61508470a8e2d65a5b1
Lala Ibrahimzade <116654316+lalaibrahimzade@users.noreply.github.com>
2024-08-13 19:17:07 +0400

    Add files via upload

---
+++ group_by.sql
@@ -0,0 +1,6 @@
+--SELECT rating, COUNT(*) FROM film GROUP BY rating;
+--SELECT replacement_cost, COUNT(*) FROM film  GROUP BY replacement_cost HAVING COUNT(*) > 50 order by replacement_cost,COUNT(*) desc;
+--SELECT store_id, COUNT(*) FROM customer GROUP BY store_id;
+--SELECT country_id, COUNT(*) FROM city GROUP BY country_id order by country_id desc limit 1;
+
+
