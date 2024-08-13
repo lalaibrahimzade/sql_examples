6a70580e39c63a8f010af3332aa0d7b7619f99df
Lala Ibrahimzade <116654316+lalaibrahimzade@users.noreply.github.com>
2024-08-13 17:52:39 +0400

    Add files via upload

---
+++ limit_offset.sql
@@ -0,0 +1,3 @@
+--SELECT title, length FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
+--SELECT title, length FROM film WHERE title LIKE '%n' ORDER BY length ASC offset 5 LIMIT 10 ;
+--SELECT first_name, last_name FROM customer WHERE store_id = 1 ORDER BY last_name DESC LIMIT 4;
