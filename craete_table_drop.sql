c10d71b906fefe6a56ee60247b984c335cedcc83
Lala Ibrahimzade <116654316+lalaibrahimzade@users.noreply.github.com>
2024-08-24 11:27:05 +0400

    Add files via upload

---
+++ craete_table_drop.sql
@@ -0,0 +1,29 @@
+-- create table author (
+--  id serial primary key,
+--  first_name varchar(50) not null,
+--  last_name varchar(50) not null,
+--  email varchar(100),
+--  birthday date
+-- );
+
+-- select * from author
+
+--insert into author (first_name, last_name, email, birthday)
+--values ('lala', 'ibrahimzade', 'lalaibra@gmail.com', '10.03.1998'),
+--		('lala', 'ibrahimzade', 'lalaibra@gmail.com', '10.03.1998');
+
+--select * from author
+
+--create table author2 (like author)
+
+--select * from author2
+
+--insert into author2 select * from author where id=2
+
+--select * from author2
+
+--create table author3 as select * from author;
+--select * from author3;
+
+--drop table if exists author4;
+--drop table if exists author2;
