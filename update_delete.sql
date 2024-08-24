bb4a777b1567eac6abfbc88185bff1ff3ec2d47e
Lala Ibrahimzade <116654316+lalaibrahimzade@users.noreply.github.com>
2024-08-24 12:37:41 +0400

    Add files via upload

---
+++ update_delete.sql
@@ -0,0 +1,6 @@
+--update author set first_name='jale', last_name='muradova', email='jalemuradova.com', birthday='1999-03-12'
+--where id=2 ;
+
+--select * from author order by id asc;
+--delete from author where id=2;
+--select * from author;
